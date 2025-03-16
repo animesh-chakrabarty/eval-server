b2c8d112c3e474620ce573a003d3fce8b081a509
animesh-chakrabarty <animeshchakrabarty.dev@gmail.com>
2025-03-16 22:02:26 +0530

    added: user registration  and OTP verification

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
+    path('verify-otp/', views.VerifyOTP.as_view(), name='verify-otp')
 ]
--- users/views.py
+++ users/views.py
@@ -13,13 +13,36 @@ class Register(APIView):
             user = serializer.save()
             OTP = util.GenerateOTP()
             OTPInstance, created = models.UserOTPMapping.objects.update_or_create(userId=user, defaults={'OTP': OTP})
-            print(OTPInstance, created)
             util.SendMail('OTP for Eval', f"Your OTP is {OTP}", [user.email])
-            return util.JsonResponse(True, serializer.data, "user registered successfully", None)
+            return util.JsonResponse(True, serializer.data, f"mail with OTP sent to {user.email} ", None)
         except ValidationError as e:
             return util.JsonResponse(False, e.detail, "validation error", None)
         except IntegrityError as e:
             return util.JsonResponse(False, str(e), "user already exists", None)
         except Exception as e:
-            print(e)
             return util.JsonResponse(False, str(e), "something went wrong", None)
+        
+class VerifyOTP(APIView):
+    def post(self, request):
+        try:
+            email = request.data.get('email')
+            OTP = request.data.get('OTP')
+            if not email or not OTP:
+                return util.JsonResponse(False, None, "Email & OTP are required", None)
+
+            user = models.User.objects.filter(email=email).first()
+            if not user:
+                return util.JsonResponse(False, None, f"User not found with email {email}", None)
+            
+            retrievedOTP = models.UserOTPMapping.objects.filter(userId=user.id).first()
+            if retrievedOTP.OTP != OTP:
+                return util.JsonResponse(False, None, "OTP doesn't match", None)
+            
+            user.is_active = True;
+            user.save()
+            retrievedOTP.delete()
+
+            return util.JsonResponse(True, None, "OTP verified successfully! Account activated", None)
+        
+        except Exception as e:
+            return util.JsonResponse(False, str(e), "Something went wrong.", None)
